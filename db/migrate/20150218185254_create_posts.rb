a8b09649a81b612126b9d103a8bad2c13d9ec173
William Mathewson <wncmathewson@me.com>
2015-02-18 21:32:04 +0000

    Typo in first migration fixed

--- db/migrate/20150218185254_create_posts.rb
+++ db/migrate/20150218185254_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.timedate :created_at
-      t.timedate :updated_at
+      t.datetime :created_at
+      t.datetime :updated_at
 
       t.timestamps null: false
     end
