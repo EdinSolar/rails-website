b8f98c143076f18d39709cc0a32ea16efa645249
William Mathewson <wncmathewson@me.com>
2015-02-20 14:27:53 +0000

    Added validations to Post to ensure fields are used

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validate :title, presence: true
+  validates_length_of :title, within: 2..127, on: :create, message: "must be present"
+  validate :body, presence: true
 end
