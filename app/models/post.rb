b7acc4908bc607a30150905a3d30d4a14b578ff1
William Mathewson <wncmathewson@me.com>
2015-02-20 15:29:48 +0000

    It's validates not validate *facepalm*

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validate :title, presence: true
+  validates :title, presence: true
   validates_length_of :title, within: 2..127, on: :create, message: "must be present"
-  validate :body, presence: true
+  validates :body, presence: true
 end
