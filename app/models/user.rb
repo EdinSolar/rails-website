91b28784790f9e71183121e260b36ee9aab931f2
William Mathewson <wncmathewson@me.com>
2015-02-22 13:57:14 +0000

    New method for roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :roles
+
+  def role?(role)
+    !roles.find_by_name(role.to_s.camelize).nil?
+  end
 end
