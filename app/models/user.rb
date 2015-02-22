96911d9de2ab1ebb978b8ee7d517021e3148996f
William Mathewson <wncmathewson@me.com>
2015-02-22 17:05:15 +0000

    Moved around some lines in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
+
+  has_and_belongs_to_many :roles
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_and_belongs_to_many :roles
+  # Virtual attribute for authenticating by either username or email
+  attr_accessor :login
 
   def role?(role)
     !roles.find_by_name(role.to_s.camelize).nil?
