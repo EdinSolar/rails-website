bc88772762663301c1c04721f829ab0b419ceeec
William Mathewson <wncmathewson@me.com>
2015-02-23 10:19:08 +0000

    New method to handle login with both email & uname

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,15 @@ class User < ActiveRecord::Base
   def role?(role)
     !roles.find_by_name(role.to_s.camelize).nil?
   end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).
+        where(['lower(username) = :value OR lower(email) = :value',
+              {value: login.downcase}]).first
+    else
+      where(conditions.to_h)
+    end
+  end
 end
