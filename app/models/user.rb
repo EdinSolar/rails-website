3be2296983fb4734a50f2f79248f4de1842ef2e8
William Mathewson <wncmathewson@me.com>
2015-02-23 11:13:24 +0000

    Actually, we don't want to use unames for pswd rec

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,18 +19,14 @@ class User < ActiveRecord::Base
     !roles.find_by_name(role.to_s.camelize).nil?
   end
 
-  def self.find_first_by_auth_conditions(warden_conditions)
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).
+      where(conditions.to_h).
         where(['lower(username) = :value OR lower(email) = :value',
               {value: login.downcase}]).first
     else
-      if conditions[:username].nil?
-        where(conditions).first
-      else
-        where(username: conditions[:username]).first
-      end
+      where(conditions.to_h)
     end
   end
 end
