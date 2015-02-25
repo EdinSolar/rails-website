8e37f6dc1f7477930eb12504befcd8821c37af7c
William Mathewson <wncmathewson@me.com>
2015-02-25 15:01:02 +0000

    Posts belong to users

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates_length_of :title, within: 2..127, on: :create, message: "must be present"
   validates :body, presence: true
+
+  belongs_to :user
 end
