080625355a5f2ab50193a1d03174fe52432b7b90
William Mathewson <wncmathewson@me.com>
2015-02-28 14:22:00 +0000

    New helper method to render the markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_markdown(text)
+    options = {no_intra_emphasis: true, fenced_code_blocks: true,
+               autolink: true, space_after_headers: true, superscript: true,
+               highlight: true, footnotes: true}
+    html_renderer = Redcarpet::Render::HTML.new(filter_html: true,
+                                                hard_wrap: true)
+    renderer = Redcarpet::Markdown.new(html_renderer, options)
+
+    renderer.render(text).html_safe
+  end
 end
